3c1db97ae0ff48f676f7e72471e17e77c9ac08d6
Dennis Köcher <dennis@nucs.eu>
2024-01-28 21:38:46 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,18 @@ from blog.forms import CommentForm
 import logging
 logger = logging.getLogger(__name__)
 
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
+
 # Create your views here.
 
+#@cache_page(300)
+#@vary_on_cookie
 def index(request):
+    #from django.http import HttpResponse
+    #logger.debug("Index function is called!")
+    #return HttpResponse(str(request.user).encode("ascii"))
+
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
